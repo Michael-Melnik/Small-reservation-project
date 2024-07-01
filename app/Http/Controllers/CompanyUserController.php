59c1d4adf3f59bb1b897e326d9a3402642f58fe3
Michael_Melnik <mikhamelnbr@yandex.ru>
2024-07-01 13:42:04 +0300

    update CompanyUserController

--- app/Http/Controllers/CompanyUserController.php
+++ app/Http/Controllers/CompanyUserController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
 use App\Models\Company;
 use App\Enums\Role;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CompanyUserController extends Controller
@@ -13,5 +16,39 @@ class CompanyUserController extends Controller
         $users = $users = $company->users()->where('role_id', '=', Role::CUSTOMER->value)->get();
 
         return view('companies.users.index', compact('company', 'users'));
+    }
+
+    public function create(Company $company)
+    {
+        return view('companies.users.create', compact('company'));
+    }
+
+    public function store(StoreUserRequest $request, Company $company)
+    {
+        $company->users()->create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+            'role_id' => Role::COMPANY_OWNER->value,
+        ]);
+        return to_route('companies.users.index', $company);
+    }
+
+    public function edit(Company $company, User $user)
+    {
+        return view('companies.users.edit', compact('company', 'user'));
+    }
+
+    public function update(UpdateUserRequest $request ,Company $company, User $user)
+    {
+        $user->update($request->validated());
+
+        return to_route('companies.users.index', $company);
+    }
+
+    public function destroy(Company $company, User $user)
+    {
+        $user->delete();
+        return to_route('companies.users.index', $company);
     }
 }
